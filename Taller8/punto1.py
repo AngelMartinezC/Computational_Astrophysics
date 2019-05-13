58d3b7f297c686b265a36c669d2b95f32b0c256e
Angel Daniel Martinez Cifuentes <andmartinezci@unal.edu.co>
2019-05-13 12:10:23 -0500

    Update punto1

--- Taller8/punto1.py
+++ Taller8/punto1.py
@@ -9,10 +9,57 @@ import matplotlib.pyplot as plt
 from astropy.io import ascii
 
 
+
+def linear(x,y,sigma=None):
+
+    if isinstance(sigma,(list,np.ndarray)):
+        s = sigma
+        for i in range(len(x)):
+            if not isinstance(s[i],(int,float)):
+                    s[i] = 1
+            else: pass
+    elif isinstance(sigma,(int,float)):
+        s = [sigma for i in range(len(x))]
+    elif sigma==None:
+        s = [1 for i in range(len(x))]
+    else: pass
+    print(s)
+
+    S,X,Y,X2,XY =0,0,0,0,0
+    for i in range(len(s)):
+        S  += 1/(s[i])**2
+        X  += x[i]/(s[i])**2
+        X2 += (x[i])**2/(s[i])**2
+        Y  += y[i]/(s[i])**2
+        XY += x[i]*y[i]/(s[i])**2
+
+    b = (Y*X2-X*XY)/(S*X2-X**2)
+    m = (S*XY-X*Y)/(S*X2-X**2)
+
+    return m, b
+
 data = ascii.read('table1.dat',readme='ReadMe')
 
-s = data['sigma*']
-M = data['logM']
+logs = np.log(data['sigma*'])
+logM = data['logM']
 
-plt.plot(s,M,'.')
-plt.show()
+err_s = data['e_sigma*']
+err_M = data['e_logM']
+Err_M = data['E_logM']
+
+print(type(Err_M[79]))
+print(type(Err_M[0]))
+print(type(Err_M[5]))
+
+
+M,   B   = linear(x=logs, y=logM)
+Mye, Bye = linear(x=logs, y=logM, sigma=err_M)
+MyE, ByE = linear(x=logs, y=logM, sigma=Err_M)
+
+x = np.linspace(1.75,2.75,1000)
+plt.plot(logs,logM,'o')
+plt.plot(x,M*x+b)
+plt.plot(x,Mye*x+Bye)
+plt.plot(x,MyE*x+ByE)
+
+print(Err_M)
