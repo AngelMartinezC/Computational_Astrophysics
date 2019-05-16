878d6d049ea8452ff0ddc540b9fc54a8354e2974
AngelMartinezC <andmartinezci@unal.edu.co>
2019-05-16 00:53:03 -0500

    Update punto1

--- Taller8/punto1.py
+++ Taller8/punto1.py
@@ -35,8 +35,11 @@ def linear(x,y,sigma=None):
 
 	b = (Y*X2-X*XY)/(S*X2-X**2)
 	m = (S*XY-X*Y)/(S*X2-X**2)
-
-	return m, b
+	
+	sb = np.sqrt(X2/(S*X2-(X)**2))
+	sm = np.sqrt(S/(S*X2-(X)**2))
+	
+	return m, b, sm, sb
 
 
 def plot(name='table1.dat',readme='ReadMe',fig='Mvssigma_noadd.png'):
@@ -50,14 +53,11 @@ def plot(name='table1.dat',readme='ReadMe',fig='Mvssigma_noadd.png'):
 	err_M = data['e_logM']
 	Err_M = data['E_logM']
 
-	M,   B   = linear(x=logs, y=logM)
-	print('M is ',M)
-	print('B is ',B)
-	print(np.log10(4.02))
-	print(np.log10(7.96))
-	Mye, Bye = linear(x=logs, y=logM, sigma=err_M)
-	MyE, ByE = linear(x=logs, y=logM, sigma=Err_M)
-
+	M,   B,   Sm,  Sb  = linear(x=logs, y=logM, sigma=err_M)
+	#Sb = 0.1832
+	
+	print(Sm,Sb)
+	
 	plt.figure(figsize=(5,5))
 	x = np.linspace(1.4,2.65,1000)
 	plt.xlim(1.4,2.65)
@@ -65,9 +65,9 @@ def plot(name='table1.dat',readme='ReadMe',fig='Mvssigma_noadd.png'):
 	plt.xticks([1.5,1.75,2,2.25,2.5])
 	plt.yticks([5,6,7,8,9])
 	plt.plot(logs,logM,'o',fillstyle='none',markersize=5)
-	plt.plot(x,M*x+B,label='linear fit')
-	plt.plot(x,Mye*x+Bye,label='low fit y')
-	plt.plot(x,MyE*x+ByE,label='high fit y')
+	plt.plot(x,M*x+B,label='linear fit y = '+str(round(M,3))+'x + '+str(round(B,3)),color='g')
+	plt.plot(x,M*x+B+Sb,'--',color='r',label='Error bar for $a_1$')
+	plt.plot(x,M*x+B-Sb,'--',color='r')
 	plt.xlabel(r'$\log (\sigma _{\star} / $ km s$^{-1})$')
 	plt.ylabel(r'$\log ($ $M_{BH}$ $/$ M$\odot $ $)$')
 	plt.legend()
@@ -75,6 +75,6 @@ def plot(name='table1.dat',readme='ReadMe',fig='Mvssigma_noadd.png'):
 	plt.show()
 
 
-plot()
-plot(name='table2.dat',readme='ReadMe2',fig='Mvssigma.png')
+plot(fig='M_vs_sigma_with_sigma.png')
+#plot(name='table2.dat',readme='ReadMe2',fig='M_vs_sigma_no_sigma_2.png')
 
